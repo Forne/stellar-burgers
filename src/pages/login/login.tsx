67cc728eaea0d65c36f5738c34020316c39a6b1d
Dmitriy Pervin <theforner@gmail.com>
2024-08-22 17:23:30 +0500

    feat: users (#1)
    * create user slice
    * add user api, auth, register, logout
    * add ProtectedRoute component
    * add profile orders

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,22 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { loginUser, selectAuth } from '../../services/slices/userSlice';
+import { useDispatch, useSelector } from '../../services/store';
 
 export const Login: FC = () => {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { error } = useSelector(selectAuth);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(loginUser({ email, password }));
   };
 
   return (
     <LoginUI
-      errorText=''
+      errorText={error?.message}
       email={email}
       setEmail={setEmail}
       password={password}
